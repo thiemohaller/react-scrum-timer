7b087af42d20d10d6b8ea2541765841bb1cd8c8c
thiemo_mac <thiemo.haller@me.com>
2022-06-07 20:30:56 +0200

    make prettier

--- src/components/CountDownTimer.tsx
+++ src/components/CountDownTimer.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 
+
 interface ICountdown {
     minutes: number
     seconds: number
@@ -9,7 +10,6 @@ const CountDownTimer = ({ minutes = 1, seconds = 0 }: ICountdown) => {
     const [time, setTime] = useState<ICountdown>({ minutes, seconds })
     const [isRunning, setIsRunning] = useState<boolean>(false)
     const [bgColor, setBgColor] = useState<string>('#20A956')
-
     const [failureMessage, setFailureMessage] = useState<string>('')
 
     const tick = () => {
@@ -17,7 +17,7 @@ const CountDownTimer = ({ minutes = 1, seconds = 0 }: ICountdown) => {
             if (time.minutes === 0 && time.seconds === 0) {
                 setIsRunning(false)
                 setBgColor('#A82820')
-                setFailureMessage('We\'re not mad, just disappointed </3')
+                setFailureMessage('We\'re not mad, just disappointed </3 😣')
                 reset()
             }
             else if (time.seconds === 0) {
@@ -33,7 +33,8 @@ const CountDownTimer = ({ minutes = 1, seconds = 0 }: ICountdown) => {
     function startOrStopTimer() {
         if (isRunning) {
             setIsRunning(false)
-            setFailureMessage('Nailed it - Someone somewehere is proud of you, probably :)')
+            setFailureMessage('Nailed it - Someone somewehere is proud of you, probably :) 😌')
+            // setFailureMessage('Nailed it - Someone somewehere is proud of you,\n probably :) 😌')
         } else {
             setIsRunning(true)
             setBgColor('#20A956')
@@ -63,24 +64,25 @@ const CountDownTimer = ({ minutes = 1, seconds = 0 }: ICountdown) => {
     })
 
     return (
-        <div style={{ backgroundColor: bgColor }} className="grid grid-rows-4 grid-flow-col gap-4">
-            <div className="row-span-4 text-white font-bold p-20 timer-custom-fontsize">
+        <div style={{ backgroundColor: bgColor }} className="grid grid-rows-2 grid-flow-col gap-4">
+            <div className="flex justify-center row-span-2 text-white font-bold p-20 timer-custom-fontsize">
                 <p>{`${time.minutes.toString().padStart(2, '0')}:${time.seconds.toString().padStart(2, '0')}`}</p>
             </div>
-            <div className="flex col-span-2 row-span-2 text-white text-3xl justify-center items-center">
-                <p>{failureMessage}</p>
+            <div className="flex row-span-1 text-white text-3xl justify-center items-center fail-message">
+                {failureMessage}
             </div>
-            <div className="flex row-span-2 col-span-2 items-center justify-center flex-row">
+
+            <div className='flex justify-evenly items-center content-start'>
                 <button type="button" onClick={() => startOrStopTimer()}
-                    className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
-                    Start
+                    className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 min-w-[120px] ">
+                    Start/Stop
                 </button>
                 <button type="button" onClick={() => pauseTimer()}
-                    className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+                    className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 min-w-[120px]">
                     Pause/Resume
                 </button>
                 <button type="button" onClick={() => stopTimer()}
-                    className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+                    className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 min-w-[120px]">
                     Stop
                 </button>
             </div>
